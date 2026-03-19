eaa1779abf9298226f666fdb22b6c86f781542c8
pablocandela4 <pco12@alu.ua.es>
2026-03-19 17:12:28 +0100

    Init de la carpeta modelos hecho

--- src/club_lectura/modelos/__init__.py
+++ src/club_lectura/modelos/__init__.py
@@ -1,1 +1,6 @@
 from .material import MaterialBibliografico
+from .libro import Libro
+from .articulo import Articulo
+from .resena import Resena
+from .bibliografia import Bibliografia
+from .sesion import SesionLectura
